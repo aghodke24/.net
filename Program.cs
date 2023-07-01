896b8d752b74f51c3112d563db948e7c58941f9f
aghodke24 <93040950+aghodke24@users.noreply.github.com>
2023-07-01 19:49:15 +0530

    Assignment2

--- Program.cs
+++ Program.cs
@@ -1,94 +1,102 @@
-﻿namespace day1assignment
+﻿using System.Transactions;
+
+namespace Assignments2
 {
     internal class Program
     {
         static void Main()
         {
-            
-            Employee emp = new Employee();
-
-            Console.WriteLine("Enter Employee Name");
-            emp.Name = Console.ReadLine();
-
-            Console.WriteLine("Enter Employee Id");
-            emp.Id = Convert.ToInt32(Console.Read());
-
-            Console.WriteLine("Enter Basis Salary");
-            emp.Basic = Convert.ToDecimal(Console.Read());
-
-            Console.WriteLine("Enter the Department Number");
-            emp.DeptNo = Convert.ToInt16(Console.Read());
-
-
-            Console.WriteLine("Employee name is : " + emp.Name + " Id is : " + emp.Id + "Basic Salary is :" + emp.Basic + "Depertment Number is :" + emp.DeptNo);
-
+            StudentDetails.Student s = new StudentDetails.Student("Ankit", 100, 56324);
+            StudentDetails.Student s1 = new StudentDetails.Student("Adwait", 201, 10024);
+            s.Display();
+            Console.WriteLine(s1.GetNetSalary(56324));
+            s1.Display();
         }
     }
+}
+//add class dept
+namespace StudentDetails
+{
+    /* internal class Depertment
+     {
+         public int DeptNo { get; set; }
 
-    class Employee
+         static void Main(string[] args)
+         {
+             Depertment depert = new Depertment();
+             Console.WriteLine("Enter dept no");
+             depert.DeptNo = Convert.ToInt32(Console.ReadLine());
+         }
+     }*/
+    internal class Student
     {
-        private int id;
+        public static int empNo;
         private string name;
-        private decimal basic;
         private short deptNo;
+        private decimal baseSalary;
+        private int empID;
 
-        public int Id
+        public Student(String name, short deptNo = 0, decimal baseSalary = 0)
         {
-            get
-            { return id; 
-            }
+            this.empID = ++empNo;
+            this.name = name;
+            this.deptNo = deptNo;
+            this.baseSalary = baseSalary;
+        }
+        public decimal BaseSalary
+        {
+            get { return baseSalary; }
             set
             {
-                if (value > 0)
-                    id = value;
+                if (value > 0 && value > 1000000)
+                    baseSalary = value;
                 else
-                    Console.WriteLine("id should be greater than 0");
+                    Console.WriteLine("Enter proper base salary");
             }
         }
-        public string Name
-        {
-            get
-            { return name;
-            }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    Console.WriteLine("name should not be null or empty");
-                else
-                    name = value;
-            }
-
-        }
-
-        public decimal Basic
-        {
-            get
-            { return basic;
-            }
-            set
-            {
-                if (value > 10000 && value < 25000)
-                    basic = value;
-                else
-                    Console.WriteLine("Basic Salary should be between 10000 & 25000 ");
-            }
-        }
-
         public short DeptNo
         {
             get
-            { return deptNo; }
+            {
+                return deptNo;
+            }
             set
             {
                 if (value > 0)
                     deptNo = value;
                 else
-                    Console.WriteLine("Departement number must be greater then 0");
+                    Console.WriteLine("valus should be greather than zero");
             }
         }
 
-    }
 
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length > 0)
+                    name = value;
+                else
+                    Console.WriteLine("name can't be null");
+            }
+        }
+
+        public decimal GetNetSalary(decimal basic)
+        {
+            return basic * (decimal)1.6;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(this.empID + " " + this.Name + " " + this.DeptNo + " " + this.BaseSalary);
+        }
+    }
 }
 
 

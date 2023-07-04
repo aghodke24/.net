974affe99c72be64954bfbc9193fc371e0adb644
aghodke24 <93040950+aghodke24@users.noreply.github.com>
2023-07-04 20:30:00 +0530

    Add files via upload

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of cdac batch");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[][] cdac = new int[size][];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter the number of students in bacth [{i}]");
+                int students = Convert.ToInt32(Console.ReadLine());
+                cdac[i] = new int[students];
+                for (int j = 0; j < students; j++)
+                {
+                    Console.WriteLine("Enter the marks for student");
+                    cdac[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"The Number of students for batch cdac[{i}] is ");
+                for (int j = 0; j < cdac[i].Length; j++)
+                {
+                    Console.WriteLine(cdac[i][j]);
+                }
+            }
+        }
+    }
+}
